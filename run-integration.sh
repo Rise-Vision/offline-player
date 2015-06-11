962d4f0251985dedc72d16fbdfa2ad1fd866c272
tejohnso <mail@tyler-johnson.ca>
2015-06-11 14:33:54 -0400

    Set up run integration npm script

---
+++ run-integration.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+../../mocha-chrome-app/run-test.sh $(pwd)/test/platform/io-provider-it.js
--- test/platform/io-provider-it.js
+++ test/platform/io-provider-it.js
@@ -1,6 +1,8 @@
+var assert = require("assert"),
+platformIO = require("../../app/player/platform/io-provider.js");
+
 describe("io provider platform functions", function() {
   it("exists", function() {
-    var platformIO = require("../../app/player/platform/io-provider.js");
-    assert.ok(platformIO, "does not exist");
+    assert.ok(platformIO, "existence");
   });
 });
