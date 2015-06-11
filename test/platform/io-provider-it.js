c8ed8e04c8fb7e5f7cffca953627235ff64ba500
tejohnso <mail@tyler-johnson.ca>
2015-06-10 20:44:21 -0400

    Add integration test for IO provider

---
+++ test/platform/io-provider-it.js
@@ -0,0 +1,6 @@
+describe("io provider platform functions", function() {
+  it("exists", function() {
+    var platformIO = require("../../app/player/platform/io-provider.js");
+    assert.ok(platformIO, "does not exist");
+  });
+});
