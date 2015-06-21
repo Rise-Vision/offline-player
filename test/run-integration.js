a4d7aaa383bf6602a57b022705b00a0eb195669e
tejohnso <mail@tyler-johnson.ca>
2015-06-21 15:45:26 -0400

    Clean up integration testing

--- test/run-integration.js
+++ test/run-integration.js
@@ -1,15 +1,8 @@
 "use strict";
 var path = require("path"),
-itFiles = [],
-serverProcess,
+itFiles = require("glob").sync("test/**/*-it.js"),
 spawn = require("child_process").spawn,
 childProcess;
-
-itFiles = [
-  path.join(process.cwd(), "test/platform/io-provider-it.js"),
-  path.join(process.cwd(), "test/platform/ui-controller-it.js"),
-  path.join(process.cwd(), "test/main/start-it.js")
-];
 
 if (process.argv[2]) {
   itFiles = [process.argv[2]];
@@ -18,7 +11,7 @@ if (process.argv[2]) {
 function integrationTestCommandOptions() {
   return ["node_modules/chrome-app-test-runner/run-test.js"]
   .concat(itFiles)
-  .concat("--mock-server=" + path.join(process.cwd(), "test/mock-server.js"));
+  .concat("--mock-server=test/mock-server.js");
 }
 
 console.log("Running test runner against " + itFiles.length + " files");
