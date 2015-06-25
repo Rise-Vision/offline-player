d28038ca5448de7a19a39cc8dec202d37960a03e
tejohnso <mail@tyler-johnson.ca>
2015-06-25 10:01:37 -0400

    Begin using simple-mock for mocking
    For the following reasons:
    
     - It's less convenient to see what the mock is doing when it's in a
       different file
     - Keeping the mock behaviour inline with each unit test is more
       accurage and more descriptive.  Each test says "mock it this way, now
    check this behaviour."
     - When modules change, the test file needs to be changed.  If the mock
       is in a separate file, that's a third file that needs to be changed.
     - Using a simple mocking framework encourages simple mocks.

--- test/cache/mock-html-parser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = function() {
-  return {
-    parseSavedHtmlFile: function(presentationUrl) {
-      var returnedUrl = "filesystem-url-to-parsed-html";
-      return returnedUrl;
-    }
-  };
-};
