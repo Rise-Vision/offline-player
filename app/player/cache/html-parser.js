7ca09def67d7035912af26b1362a35ac5af2f80c
tejohnso <mail@tyler-johnson.ca>
2015-06-29 18:20:55 -0400

    Parse all html files

--- app/player/cache/html-parser.js
+++ app/player/cache/html-parser.js
@@ -1,33 +1,65 @@
-cheerio = require("cheerio");
+var cheerio = require("cheerio"),
+path = require("path");
 
 module.exports = function(platformIO) {
   return {
-    parseSavedHtmlFile: function(url) {
-      var mainUrlPath = url.substr(0, url.lastIndexOf("/") + 1),
-      folderItems;
+    parseFiles: function() {
+      var folderItems;
 
       return platformIO.localObjectStore.get(["folderItems"])
       .then(function(items) {
-        folderItems = items.folderItems[mainUrlPath];
-      })
-      .then(function() {
-        return platformIO.filesystemRetrieve(url, {includeContents: true});
-      })
-      .then(function(resp) {
-        var $ = cheerio.load(resp.fileContentString);
-
-        $("*[href]").attr("href", internalizeExternalReferences);
-        $("*[src]").attr("src", internalizeExternalReferences);
-        return platformIO.filesystemSave("PARSED" + url, $.html());
+        if (!items.folderItems) {return;}
+        folderItems = items.folderItems;
+        return Promise.all(Object.keys(folderItems).map(mainUrlProcessor));
       });
 
-      function internalizeExternalReferences(idx, extRef) {
-        var storedEntry = folderItems[mainUrlPath + extRef] || folderItems[extRef];
-        if (storedEntry) {
-          return storedEntry.localUrl;
-        }
+      function mainUrlProcessor(mainUrlPath) {
+        var filePaths = Object.keys(folderItems[mainUrlPath]).filter(function(key) {
+          return /\.html$/.test(key);
+        });
 
-        return extRef;
+        return Promise.all
+        (filePaths.map(createSaveItemFunction(mainUrlPath)))
+        .then(function() {
+          return Promise.all
+          (filePaths.map(createParseItemFunction(mainUrlPath)));
+        });
+      }
+
+      function createSaveItemFunction(mainUrlPath) {
+        return function(filePath) {
+          return platformIO.filesystemSave
+          ("PARSED" + mainUrlPath + filePath, "to-be-parsed")
+          .then(function(resp) {
+            folderItems[mainUrlPath][filePath].localUrl = resp;
+          });
+        };
+      }
+
+      function createParseItemFunction(mainUrlPath) {
+        return function(filePath) {
+          return platformIO.filesystemRetrieve
+          (mainUrlPath + filePath, {includeContents: true})
+          .then(function(resp) {
+            var $ = cheerio.load(resp.fileContentString);
+
+            $("*[href]").attr("href", internalizeExternalReferences);
+            $("*[src]").attr("src", internalizeExternalReferences);
+            return platformIO.filesystemSave("PARSED" + mainUrlPath + filePath, $.html());
+          });
+
+          function internalizeExternalReferences(idx, extRef) {
+            var filePathDir = path.dirname(filePath);
+            if (filePathDir === ".") {filePathDir = "";}
+
+            var storedEntry = folderItems[mainUrlPath][path.join(filePathDir, extRef)] || folderItems[extRef];
+            if (storedEntry) {
+              return storedEntry.localUrl;
+            }
+
+            return extRef;
+          }
+        };
       }
     }
   };
--- test/cache/html-parser-test.js
+++ test/cache/html-parser-test.js
@@ -1,18 +1,26 @@
 "use strict";
 var assert = require("assert"),
 mock = require("simple-mock").mock,
+platformIO,
 parser;
 
 describe("html parser", function() {
   beforeEach("setup mocks", function() {
-    var platformIO = {localObjectStore: {}};
+    platformIO = {localObjectStore: {}};
     mock(platformIO.localObjectStore, "get").resolveWith({folderItems: {
-      "http://external-url/": []
+      "http://external-url1/": {
+        "externalFile1.html": {"localUrl": "orig-local-url"},
+        "externalFile2.jpg": {"localUrl": "orig-local-url"}
+      },
+      "http://external-url2/": {
+        "externalFile3.txt": {"localUrl": "orig-local-url"},
+        "myDir/externalFile4.html": {"localUrl": "orig-local-url"}
+      }
     }});
-    mock(platformIO, "filesystemSave").returnWith("local-url");
+    mock(platformIO, "filesystemSave").resolveWith("local-url");
     mock(platformIO, "filesystemRetrieve").resolveWith({
       url: "local-url",
-      fileContentString: "<html><img src='folder/file.txt' /></html>"
+      fileContentString: "<img src='externalFile4.html' />"
     });
     
     parser = require("../../app/player/cache/html-parser.js")(platformIO);
@@ -22,10 +30,33 @@ describe("html parser", function() {
     assert.ok(parser);
   });
 
-  it("returns a url to the parsed html", function() {
-    return parser.parseSavedHtmlFile("http://external-url/main.html")
-    .then(function(returnedUrl) {
-      assert.equal(returnedUrl, "local-url");
+  it("retrieves previously saved folder items", function() {
+    return parser.parseFiles()
+    .then(function() {
+      assert.deepEqual
+      (platformIO.localObjectStore.get.calls[0].args[0], ["folderItems"]);
+    });
+  });
+
+  it("resaves a new version of all parseable files", function() {
+    return parser.parseFiles()
+    .then(function() {
+      assert.equal(platformIO.filesystemSave.callCount, 4);
+      assert(platformIO.filesystemSave.calls.some(function(call) {
+        console.log(call.args[0]);
+        return call.args[0] === "PARSEDhttp://external-url2/myDir/externalFile4.html"; 
+      }));
+    });
+  });
+
+  it("parses files", function() {
+    return parser.parseFiles()
+    .then(function() {
+      assert.equal(platformIO.filesystemRetrieve.callCount, 2);
+      assert(platformIO.filesystemSave.calls.some(function(call) {
+        return /PARSED.*/.test(call.args[0]) &&
+        call.args[1] === '<img src="local-url">';
+      }));
     });
   });
 });
