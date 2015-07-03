12553ab84e5627c1f8a99feb263d95c7b007dded
Francisco Vallarino <fjvallarino@gmail.com>
2015-07-03 14:50:07 -0300

    Fix istanbul path issue on Windows

--- test/run-unittest.js
+++ test/run-unittest.js
@@ -1,13 +1,15 @@
 "use strict";
-var mochaPath, ret;
+var istanbul, mochaPath, ret;
 
 if (process.platform === "win32") {
+  istanbul = "istanbul.cmd";
   mochaPath = "node_modules/mocha/bin/_mocha";
 } else {
+  istanbul = "istanbul";
   mochaPath = "_mocha";
 }
 
-ret = require("child_process").spawnSync("istanbul", ["cover", "--", mochaPath, "--colors", "-t", "5000", "test/*-test.js", "test/**/*-test.js"]);
+ret = require("child_process").spawnSync(istanbul, ["cover", "--", mochaPath, "--colors", "-t", "5000", "test/*-test.js", "test/**/*-test.js"]);
 
 console.log(ret.stdout.toString());
 console.log(ret.stderr.toString());
