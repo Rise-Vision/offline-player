9e3a6a70d8f3c156c50d9469e6782042ee63ef5a
tejohnso <mail@tyler-johnson.ca>
2015-07-22 10:24:35 -0400

    Fix options page DI

--- app/player/options/options-page-init.js
+++ app/player/options/options-page-init.js
@@ -4,75 +4,77 @@ window.addEventListener("load", function() {
   serviceUrls,
   controller;
 
-  serviceUrls = require("./service-urls.js")(platformInfo, bigQueryCredentials),
-  controller = require("./options-page-controller.js")(serviceUrls);
-  platformInfo.initPlatform();
+  platformInfo.initPlatform()
+  .then(function() {
+    serviceUrls = require("./service-urls.js")(platformInfo, bigQueryCredentials);
+    controller = require("./options-page-controller.js")(serviceUrls);
 
-  (function setOptionsWindowCloseTimeout() {
-    var timeoutHandle;
+    (function setOptionsWindowCloseTimeout() {
+      var timeoutHandle;
 
-    timeoutHandle = setTimeout(function() {window.close();}, 10000);
+      timeoutHandle = setTimeout(function() {window.close();}, 10000);
 
-    document.addEventListener("click", function() {
-      clearTimeout(timeoutHandle);
-    });
-  }());
+      document.addEventListener("click", function() {
+        clearTimeout(timeoutHandle);
+      });
+    }());
 
-  controller.setDimensions({width: screen.width, height: screen.height});
+    controller.setDimensions({width: screen.width, height: screen.height});
 
-  (function restoreUIFields() {
-    chrome.storage.local.get(Object.keys(controller.getUIValues()), function(values) {
-      controller.setUIValues(values);
-      initializeSections();
-    });
-  }());
+    (function restoreUIFields() {
+      chrome.storage.local.get(Object.keys(controller.getUIValues()), function(values) {
+        controller.setUIValues(values);
+        initializeSections();
+      });
+    }());
 
-  controller.setUIFieldMapObserver(function(changes) {
-    var latestChanges = changes.pop().object;
+    controller.setUIFieldMapObserver(function(changes) {
+      var latestChanges = changes.pop().object;
 
-    for (var elementId in latestChanges) {
-      document.getElementById(elementId).value = latestChanges[elementId];
-    }
-
-    document.getElementById("displayNameReady").value = latestChanges.displayName;
-    document.getElementById("displayIdReady").value = latestChanges.displayId;
-
-    chrome.storage.local.set(latestChanges, function() {
-      if (chrome.runtime.lastError) {
-        controller.setUIStatus
-        ({message: "Could not save info", severity: "severe"});
+      for (var elementId in latestChanges) {
+        document.getElementById(elementId).value = latestChanges[elementId];
       }
+
+      document.getElementById("displayNameReady").value = latestChanges.displayName;
+      document.getElementById("displayIdReady").value = latestChanges.displayId;
+
+      chrome.storage.local.set(latestChanges, function() {
+        if (chrome.runtime.lastError) {
+          controller.setUIStatus
+          ({message: "Could not save info", severity: "severe"});
+        }
+      });
     });
-  });
 
-  controller.setUIStatusObserver(function(changes) {
-    document.getElementById("status").dispatchEvent
-    (new CustomEvent("status.update", {detail: changes}));
-  });
+    controller.setUIStatusObserver(function(changes) {
+      document.getElementById("status").dispatchEvent
+      (new CustomEvent("status.update", {detail: changes}));
+    });
 
-  (function bindDomInputListeners() {
-    addListener("close", "click", function() {window.close();});
-    addListener("displayIdApply", "click", require("./display-id-click-listener.js")(controller));
-    addListener("claimIdApply", "click", require("./claim-id-click-listener.js")(controller));
-    addListener("changeDisplayIdApply", "click", require("./change-display-id-click-listener.js")(controller));
-    addListener("status", "status.update", require("./status-field-listener.js"));
+    (function bindDomInputListeners() {
+      addListener("close", "click", function() {window.close();});
+      addListener("displayIdApply", "click", require("./display-id-click-listener.js")(controller));
+      addListener("claimIdApply", "click", require("./claim-id-click-listener.js")(controller));
+      addListener("changeDisplayIdApply", "click", require("./change-display-id-click-listener.js")(controller));
+      addListener("status", "status.update", require("./status-field-listener.js"));
 
-    function addListener(id, evt, fn) {
-      document.getElementById(id).addEventListener(evt, fn);
+      function addListener(id, evt, fn) {
+        document.getElementById(id).addEventListener(evt, fn);
+      }
+    }());
+
+    function initializeSections() {
+      var uiValues = controller.getUIValues();
+
+      if(!uiValues.displayId) {
+        controller.setUIValues({
+          claimId: "",
+          displayName: ""
+        });      
+      }
+
+      document.getElementById("displayReadySection").style.display = uiValues.displayId ? "block" : "none";
+      document.getElementById("displaySetupSection").style.display = uiValues.displayId ? "none" : "block";
     }
-  }());
-
-  function initializeSections() {
-    var uiValues = controller.getUIValues();
-
-    if(!uiValues.displayId) {
-      controller.setUIValues({
-        claimId: "",
-        displayName: ""
-      });      
-    }
-
-    document.getElementById("displayReadySection").style.display = uiValues.displayId ? "block" : "none";
-    document.getElementById("displaySetupSection").style.display = uiValues.displayId ? "none" : "block";
-  }
+  });
 });
